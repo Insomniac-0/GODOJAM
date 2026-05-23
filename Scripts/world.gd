85ff7ad7a138316885f4ec8e85d83dbcbad9e605
Insomniac <Insomniac-0@users.noreply.github.com>
2026-05-23 02:44:30 +0200

    bats

---
+++ Scripts/world.gd
@@ -0,0 +1,5 @@
+extends Node
+@onready var player = $Player
+
+func _physics_process(delta: float) -> void:
+	get_tree().call_group("Enemies", "update_target_location", player.global_transform.origin)
