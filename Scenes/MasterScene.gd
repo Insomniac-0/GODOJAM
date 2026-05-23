b3b24ac78192168a0106d60e4963ed3c1c9b6382
detdu <jeremia.karlemo@gmail.com>
2026-05-23 21:16:53 +0200

    SCene transitions

---
+++ Scenes/MasterScene.gd
@@ -0,0 +1,6 @@
+extends Node
+
+@export var initialScene : StringName = &""
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready() -> void:
+	SceneLoader.load_scene(initialScene)
